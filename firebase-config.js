6f455b4b7aac6a48eab8fe5c8c989a375ca3906e
motherduckkitchen <adamnorris80@gmail.com>
2026-03-18 16:32:17 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,11 @@
-window.MDK_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAt0rXQIIZI2MxS0a6b6ObixF0Bf-UiiPI",
-  authDomain: "md-kitchen-live.firebaseapp.com",
-  projectId: "md-kitchen-live",
-  storageBucket: "md-kitchen-live.firebasestorage.app",
-  messagingSenderId: "552178339796",
-  appId: "1:552178339796:web:14ca54af158fd40e089041"
-};
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyD9dVq_Jkb0H5UkVKxTjZiadulqdAMg4R8",
+    authDomain: "md-kitchen-live.firebaseapp.com",
+    projectId: "md-kitchen-live",
+    storageBucket: "md-kitchen-live.firebasestorage.app",
+    messagingSenderId: "552178339796",
+    appId: "1:552178339796:web:f1041a3d98955c96089041",
+    measurementId: "G-VGY8KXJBS3"
+  };
+</script>
