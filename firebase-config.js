491ee5b0b54d3cf37d2a818747c4d709bf656170
motherduckkitchen <adamnorris80@gmail.com>
2026-03-18 14:19:00 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD9dVq_Jkb0H5UkVKxTjZiadulqdAMg4R8",
+window.MDK_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAt0rXQIIZI2MxS0a6b6ObixF0Bf-UiiPI",
   authDomain: "md-kitchen-live.firebaseapp.com",
   projectId: "md-kitchen-live",
   storageBucket: "md-kitchen-live.firebasestorage.app",
   messagingSenderId: "552178339796",
-  appId: "1:552178339796:web:f1041a3d98955c96089041",
-  measurementId: "G-VGY8KXJBS3"
+  appId: "1:552178339796:web:14ca54af158fd40e089041"
 };
